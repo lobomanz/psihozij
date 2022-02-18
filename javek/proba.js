475fb2cb1aed29da84df3fb141e50ee664dd9b15
lobomanz <lovro.monkey@gmail.com>
2022-02-18 15:25:44 +0100

    komit

---
+++ javek/proba.js
@@ -0,0 +1,11 @@
+let images = ['#jedan', `#dva`, `#tri`, `#cetri`];
+let jedan= document.getElementById('jedan');
+let dva= document.getElementById('dva');
+let tri= document.getElementById('tri');
+let cetri= document.getElementById('cetri');
+let x=0;
+let y=screen.height;
+
+
+
+
