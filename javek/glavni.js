695dff9a6ac9b46828770157da7f3c708c4e818f
lobomanz <lovro.monkey@gmail.com>
2022-03-06 01:15:06 +0100

    obrada

--- javek/glavni.js
+++ javek/glavni.js
@@ -1,22 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const hamburger = document.querySelector(".hamburger");
+const Menu = document.querySelector(".hamb-menu");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-
-
+hamburger.addEventListener("click", ()=>{
+  hamburger.classList.toggle("active");
+  Menu.classList.toggle("active");
+})
 
